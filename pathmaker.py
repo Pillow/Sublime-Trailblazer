2d9b809c48da8fd4e744822e317fd93f8c69aac7
Corbin Schwartz <corbin.schwartz@gmail.com>
2016-03-18 13:20:45 -0700

    modularized pathmaker

--- pathmaker.py
+++ pathmaker.py
@@ -1,50 +1,44 @@
 import sys, os, errno
-class PathMaker:
-
-  def __init__(self, current_file, relative_path):
-    self.current_file = current_file
-    self.relative_path = relative_path
-
-  def makePath(path):
-      try:
-          os.makedirs(path)
-      except OSError as exc:
-          if exc.errno == errno.EEXIST and os.path.isdir(path):
-              pass
-          else:
-              raise
-
-  def makeFile(path):
-    if os.path.exists(path) == False:
-      file = open(path, 'w')
-      file.close
-
-  def splitPath(path):
-    folders = []
-    while 1:
-        path, folder = os.path.split(path)
-        if folder != "":
-            folders.append(folder)
+def makePath(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc:
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
         else:
-            if path != "":
-                folders.append(path)
+            raise
 
-            break
-    folders.reverse()
-    return folders
+def makeFile(path):
+  if os.path.exists(path) == False:
+    file = open(path, 'w')
+    file.close
 
-  def parsePath():
-    curr_parts = splitPath(self.current_file)[:-1]
-    rel_parts = splitPath(self.relative_path)
-    for part in rel_parts:
-      if part == '..':
-        curr_parts = curr_parts[:-1]
-      elif part !='.':
-        curr_parts.append(part)
-    return os.sep.join(curr_parts)[1:]
+def splitPath(path):
+  folders = []
+  while 1:
+      path, folder = os.path.split(path)
+      if folder != "":
+          folders.append(folder)
+      else:
+          if path != "":
+              folders.append(path)
 
-  def makeFilePath():
-    newpath = parsePath(self.current_file, self.relative_path)
-    split = os.path.split(newpath)
-    makePath(split[0])
-    makeFile(newpath)
+          break
+  folders.reverse()
+  return folders
+
+def parsePath(current_file, relative_path):
+  curr_parts = splitPath(current_file)[:-1]
+  rel_parts = splitPath(relative_path)
+  for part in rel_parts:
+    if part == '..':
+      curr_parts = curr_parts[:-1]
+    elif part !='.':
+      curr_parts.append(part)
+  return os.sep.join(curr_parts)[1:]
+
+def makeFilePath(current_file, relative_path):
+  newpath = parsePath(current_file, relative_path)
+  split = os.path.split(newpath)
+  makePath(split[0])
+  makeFile(newpath)
