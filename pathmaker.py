636e363c1c26d930127da934cec18ca1cfb014b3
Corbin Schwartz <corbin.schwartz@gmail.com>
2016-03-18 14:53:35 -0700

    refactor

--- pathmaker.py
+++ pathmaker.py
@@ -40,11 +40,5 @@ def parsePath(current_file, relative_path):
 def makeFilePath(current_file, relative_path):
   newpath = parsePath(current_file, relative_path)
   split = os.path.split(newpath)
-  current_file_name, current_file_extension = os.path.splitext(current_file)
-  new_file_name, new_file_extension = os.path.splitext(relative_path)
   makePath(split[0])
-
-  if new_file_extension:
-    makeFile(newpath)
-  else:
-    makeFile("{}{}".format(newpath, current_file_extension))
+  os.path.splitext(relative_path[1]) and makeFile(newpath) or makeFile(newpath + os.path.splitext(current_file)[1])
